e58f89cda5a476c1ff4900189945e582db1c456e
Juha Karhusaari <juha.karhusaari@gmail.com>
2020-08-12 13:32:17 +0300

    created component PatientInfoPage + route & PatientListPage patient name links to info pag

---
+++ src/PatientInfoPage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import axios from "axios";
+
+const PatientInfoPage: React.FC = () => {
+    return (
+        <div>
+            <p>Patient Info</p>
+        </div>
+    );
+};
+
+export default PatientInfoPage;
