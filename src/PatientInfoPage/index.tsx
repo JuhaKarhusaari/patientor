11cf6f8cde95e04b6fffcf55bc385f1a58b7cdea
Juha Karhusaari <juha.karhusaari@gmail.com>
2020-08-13 09:12:28 +0300

    created PatientInfoPage functionality

--- src/PatientInfoPage/index.tsx
+++ src/PatientInfoPage/index.tsx
@@ -1,10 +1,68 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import { Container, Icon, Header } from "semantic-ui-react";
+
+
+import { Gender, Patient } from "../types";
+import { apiBaseUrl } from '../constants';
+
+type GenderIconCode = "mars" | "venus" | "genderless";
+
 
 const PatientInfoPage: React.FC = () => {
+    const [state, dispatch] = useStateValue();
+    const { id } = useParams<{ id: string }>();
+    const findPatient = state.patients[id];
+
+    React.useEffect(() => {
+        console.log("Patient Info");
+        axios.get<void>(`${apiBaseUrl}/ping`);
+        if (typeof findPatient !== 'undefined'){
+            if (!findPatient.ssn) {
+                const fetchPatientList = async () => {
+                    try {
+                        const { data: patientDataFromApi } = await axios.get<Patient>(
+                            `${apiBaseUrl}/patients/${id}`
+                        );
+                        
+                        dispatch({ type: "UPDATE_PATIENT", payload: patientDataFromApi });
+                    } catch (e) {
+                        console.error(e);
+                    }
+                };
+                fetchPatientList();
+        }
+        
+        }
+    }, [dispatch, findPatient, id]);
+
+    const whichGenderIcon = (gender: Gender): GenderIconCode => {
+        switch (gender) {
+            case Gender.Male:
+                return "mars";
+            case Gender.Female:
+                return "venus";
+            case Gender.Other:
+                return "genderless";
+            default:
+                return "genderless";
+        }
+    };
+
+    if (typeof findPatient === 'undefined'){
+        return <div>Loading..</div>;
+    }
+
     return (
-        <div>
-            <p>Patient Info</p>
+        <div className="App">
+            <Container textAlign="left">
+                <Header as="h3">{findPatient.name}<Icon name={whichGenderIcon(findPatient.gender)} size='large' /></Header>
+                {!findPatient.ssn ? <p></p> : <p>{`ssn : ${findPatient.ssn}`}</p>}
+                <p>{`occupation : ${findPatient.occupation}`}</p>
+                <p>{`data of birth : ${findPatient.dateOfBirth}`}</p>
+            </Container>
         </div>
     );
 };
